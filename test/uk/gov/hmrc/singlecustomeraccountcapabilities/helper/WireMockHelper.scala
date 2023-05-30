f0eb456359307d2db727f222164d9a57f119a839
cemyeniceri <cem.yeniceri@digital.hmrc.gov.uk>
2023-05-30 13:49:35 +0100

    FS-1097 refactoring

--- test/uk/gov/hmrc/singlecustomeraccountcapabilities/helper/WireMockHelper.scala
+++ test/uk/gov/hmrc/singlecustomeraccountcapabilities/helper/WireMockHelper.scala
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package helper
+package uk.gov.hmrc.singlecustomeraccountcapabilities.helper
 
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig
